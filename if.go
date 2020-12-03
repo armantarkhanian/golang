2928904e0f94ca0bf9d6e776732cdbaafbfa90da
Arman Tarkhanian <72862404+armantarkhanian@users.noreply.github.com>
2020-12-03 10:37:43 +0300

    Create if.go

---
+++ if.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x = 10
+
+	if x == 0 {
+		fmt.Println("x = 0")
+	} else {
+		if x == 1 {
+			fmt.Println("x = 1")
+		} else {
+			if x == 2 {
+				fmt.Println("x = 2")
+			} else {
+				if x == 3 {
+					fmt.Println("x = 3")
+				} else {
+					if x == 4 {
+						fmt.Println("x = 4")
+					} else {
+						if x == 5 {
+							fmt.Println("x = 5")
+						} else {
+							if x == 6 {
+								fmt.Println("x = 6")
+							} else {							
+								if x == 7 {
+									fmt.Println("x = 7")
+								} else {
+									if x == 8 {
+										fmt.Println("x = 8")
+									} else {
+										if x == 9 {
+											fmt.Println("x = 9")
+										} else {
+											if x == 10 {
+												fmt.Println("x = 10")
+											} else {
+												fmt.Println("Неизвестное число")								
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
